a1cd6791f20cbc2f5a5c37069f62286cace247ae
tegscramp <tegscramp@gmail.com>
2014-07-29 15:59:46 +0300

    Create svn.bat

---
+++ svn.bat
@@ -0,0 +1,7 @@
+
+
+TortoiseProc /command:update /path:"d:\proj\svn"
+TortoiseProc /command:commit /path:"d:\proj\svn"   
+
+TortoiseProc /command:update /path:"%~dp0"
+TortoiseProc /command:commit /path:"%~dp0"
